2ac9fc79f65f2591e7dcaf752077496a7a56924b
zhao-rgb <1019916061@qq.com>
2020-11-14 19:13:41 +0800

    配置logback日志与多环境profile

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.HelloControllerApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  **/
 @RestController
+@Slf4j
 public class HelloController implements HelloControllerApi {
 
     @Override
     public Object hello() {
+        log.info("info: hello");
+        log.info("warn: hello");
+        log.info("error: hello");
         return "hello";
     }
 }
