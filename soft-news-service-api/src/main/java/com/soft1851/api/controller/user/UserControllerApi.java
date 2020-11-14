f3423d75a7bec99af8ff5799f5327a7576b4d540
zhao-rgb <1019916061@qq.com>
2020-11-14 21:41:01 +0800

    Swagger2接口文档工具的使用

--- soft-news-service-api/src/main/java/com/soft1851/api/controller/user/UserControllerApi.java
+++ soft-news-service-api/src/main/java/com/soft1851/api/controller/user/UserControllerApi.java
@@ -1,6 +1,11 @@
 package com.soft1851.api.controller.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.soft1851.result.GraceResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author zhao
@@ -9,27 +14,20 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @Date 2020/11/14
  * @Version 1.0
  **/
-//@Api(value = "用户信息相关Controller",tags = {"用户信息相关Controller"})
-//@RequestMapping("user")
+@Api(value = "用户信息相关Controller",tags = {"用户信息相关Controller"})
+@RequestMapping("user")
 public interface UserControllerApi {
 
     /**
-     * 获取所有用户
+     * 获取所有用户信息
      * @return
      */
-    @GetMapping("/users")
-    Object getUsers();
+    @ApiOperation(value = "获得所有用户信息",notes = "获得所有用户信息",httpMethod = "POST")
+    @PostMapping("/all")
+    GraceResult getAllUsers();
 
-//    /**
-//     * 获取所有用户信息
-//     * @return
-//     */
-//    @ApiOperation(value = "获得所有用户信息",notes = "获得所有用户信息",httpMethod = "POST")
-//    @PostMapping("/all")
-//    GraceResult getAllUsers();
-//
-//    @ApiOperation(value = "获得用户基本信息",notes = "获得用户基本信息",httpMethod = "POST")
-//    @PostMapping("/userInfo")
-//    GraceResult getUserInfo(@RequestParam String userId);
+    @ApiOperation(value = "获得用户基本信息",notes = "获得用户基本信息",httpMethod = "POST")
+    @PostMapping("/userInfo")
+    GraceResult getUserInfo(@RequestParam String userId);
 
 }
--- soft-news-service-user/src/main/java/com/soft1851/user/controller/UserController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.UserControllerApi;
+import com.soft1851.pojo.AppUser;
 import com.soft1851.result.GraceResult;
 import com.soft1851.user.mapper.AppUserMapper;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 
@@ -20,17 +22,17 @@ public class UserController implements UserControllerApi {
     private AppUserMapper appUserMapper;
 
     @Override
-    public Object getUsers() {
+    public GraceResult getAllUsers() {
         return GraceResult.ok(appUserMapper.selectAll());
     }
 
 
-//    @Override
-//    public GraceResult getUserInfo(String userId) {
-//        Example userExample = new Example(AppUser.class);
-//        Example.Criteria userCriteria = userExample.createCriteria();
-//        userCriteria.andEqualTo("id",userId);
-//        AppUser user = appUserMapper.selectOneByExample(userExample);
-//        return GraceResult.ok(user);
-//    }
+    @Override
+    public GraceResult getUserInfo(String userId) {
+        Example userExample = new Example(AppUser.class);
+        Example.Criteria userCriteria = userExample.createCriteria();
+        userCriteria.andEqualTo("id",userId);
+        AppUser user = appUserMapper.selectOneByExample(userExample);
+        return GraceResult.ok(user);
+    }
 }
