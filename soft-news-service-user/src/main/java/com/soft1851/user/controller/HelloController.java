11d87a51f3aa8aace025d5aecbbcd16deec0e6cb
zhao-rgb <1019916061@qq.com>
2020-11-15 21:45:21 +0800

    安装配置整合redis

--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -2,7 +2,10 @@ package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.HelloControllerApi;
 import com.soft1851.result.GraceResult;
+import com.soft1851.utils.RedisOperator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,10 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class HelloController implements HelloControllerApi {
+    @Autowired
+    private RedisOperator redis;
 
     @Override
     public Object hello() {
         log.info("info: hello");
         return GraceResult.ok("hello");
+    }
+
+    @GetMapping("/redis")
+    public GraceResult redis() {
+        redis.set("age","21");
+        return GraceResult.ok(redis.get("age"));
     }
 }
