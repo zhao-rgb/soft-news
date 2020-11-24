ccbe34a19e3b353e0c837548565621eb137e9d9b
zhao-rgb <1019916061@qq.com>
2020-11-24 13:53:13 +0800

    文章服务模块准备

---
+++ soft-news-service-article/src/main/java/com/soft1851/article/ArticleApplication.java
@@ -0,0 +1,19 @@
+package com.soft1851.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhao
+ * @className ArticleApplication
+ * @Description TODO
+ * @Date 2020/11/24
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class ArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class,args);
+    }
+}
